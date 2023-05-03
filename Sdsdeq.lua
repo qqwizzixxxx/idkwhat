bc9f695db5b1a67513ee5c947e4b327d916ea8ed
Qwiz <125666833+qqwizzixxxx@users.noreply.github.com>
2023-05-03 16:20:27 +0300

    Create Sdsdeq.lua

---
+++ Sdsdeq.lua
@@ -0,0 +1,2 @@
+getgenv().Key = "7hFUsGVXaQ73mxgNjQJZjp4SEXy9yPdzN4bH38PdrYNBTkQw3nH2Y2mqKFcFkS52";
+loadstring(game:HttpGet('https://raw.githubusercontent.com/zReal-King/Knife-Ability-Test/main/Gui'))()
